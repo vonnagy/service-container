e51b4e56871b8161187abae4ab1a87fee5c96ab6
Ivan von Nagy <ivan@vadio.com>
2015-06-23 16:04:23 -0700

    Allow for override JSON formats

--- service-container/src/main/scala/com/github/vonnagy/service/container/http/DefaultMarshallers.scala
+++ service-container/src/main/scala/com/github/vonnagy/service/container/http/DefaultMarshallers.scala
@@ -1,5 +1,6 @@
 package com.github.vonnagy.service.container.http
 
+import net.liftweb.json.Formats
 import net.liftweb.json.ext.JodaTimeSerializers
 import spray.http._
 import spray.httpx.LiftJsonSupport
@@ -7,15 +8,16 @@ import spray.httpx.marshalling.{BasicMarshallers, Marshaller, MetaMarshallers}
 
 trait DefaultMarshallers extends MetaMarshallers with BasicMarshallers {
 
-  val lift = new LiftJsonSupport {
-    implicit def liftJsonFormats = net.liftweb.json.DefaultFormats ++ JodaTimeSerializers.all
+  def liftJson(implicit fmt: Formats): LiftJsonSupport = new LiftJsonSupport {
+    implicit def liftJsonFormats = fmt
   }
 
-  implicit def jsonFormats = lift.liftJsonFormats
+  // The implicit formats used for serialization. This can be overridden
+  implicit def jsonFormats = net.liftweb.json.DefaultFormats ++ JodaTimeSerializers.all
 
-  def jsonUnmarshaller[T: Manifest] = lift.liftJsonUnmarshaller[T]
+  def jsonUnmarshaller[T: Manifest] = liftJson.liftJsonUnmarshaller[T]
 
-  def jsonMarshaller[T <: AnyRef] = lift.liftJsonMarshaller
+  def jsonMarshaller[T <: AnyRef] = liftJson.liftJsonMarshaller
 
   def plainMarshaller[T <: Any] =
     Marshaller.delegate[T, String](ContentTypes.`text/plain`)(_.toString)
