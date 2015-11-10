78b776cb59caeb82b0d5dad058fc70b9e20479ed
Ivan von Nagy <ivan@vonnagy.net>
2015-11-10 07:40:39 -0800

    Preparing for 1.0.5

--- project/ContainerBuild.scala
+++ project/ContainerBuild.scala
@@ -16,7 +16,7 @@ object ContainerBuild extends Build {
   lazy val baseSettings = Seq(
     name := "Service Container",
     organization := "com.github.vonnagy",
-    version := "1.0.4",
+    version := "1.0.5-SNAPSHOT",
     description := "Service Container",
     scalaVersion := "2.11.6",
     crossScalaVersions := Seq("2.10.5", "2.11.6"),
