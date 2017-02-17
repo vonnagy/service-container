789f7c4036bd289ed59f7fc117e592e2cb07e80a
Alex Silva <alex-silva@pluralsight.com>
2017-02-17 12:12:01 -0500

    bumping version

--- project/ContainerBuild.scala
+++ project/ContainerBuild.scala
@@ -23,7 +23,7 @@ object ContainerBuild extends Build {
   lazy val baseSettings = Seq(
     name := "Service Container",
     organization := "com.github.vonnagy",
-    version := "2.0.2",
+    version := "2.0.3",
     description := "Service Container",
     scalaVersion := SCALA_VERSION,
     packageOptions in (Compile, packageBin) +=
